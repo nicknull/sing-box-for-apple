c31ce3daeb27b90292801e6e07e03467d8eb769b
nicknull <20854926@qq.com>
2025-10-06 20:43:58 +0800

    chore(login-consent): default consentAgreed to true for first launch

--- SharedUserKit/Views/LoginView.swift
+++ SharedUserKit/Views/LoginView.swift
@@ -48,7 +48,7 @@ struct LoginView: View {
     @AppStorage(ConstantKey.password, store: .standard) private var password  = ""
     @State var showLink:Bool = false
     @State var link:URL?
-    @AppStorage(ConstantKey.consentAgreed) private var agreed: Bool = false
+    @AppStorage(ConstantKey.consentAgreed) private var agreed: Bool = true
     
     // 不随键盘移动页面；通过可滚动表单避免遮挡
 
