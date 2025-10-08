6b3bc3ddee438fdcc0b3e7911f199f20a82b2e48
nicknull <20854926@qq.com>
2025-10-08 20:09:59 +0800

    upd：更新接口实现

--- SharedUserKit/Models/TestEnv.swift
+++ SharedUserKit/Models/TestEnv.swift
@@ -5,6 +5,6 @@ public enum TestEnv {
     public static let isEnabled: Bool = true
 
     // 固定的测试环境 Host（需要时可修改为你的测试地址）
-    public static let fixedHost: String = "http://test.erewwd.shop/"
+    public static let fixedHost: String = "http://test.erewwd.shop"
 }
 
