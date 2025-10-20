fafcba8463adde035860304fa17af74e730063f7
nicknull <20854926@qq.com>
2025-10-20 21:05:55 +0800

    chore: bump build number

--- SharedUserKit/Views/UserView.swift
+++ SharedUserKit/Views/UserView.swift
@@ -15,6 +15,7 @@ import CodeScanner
 #if DEBUG
 import SharedCrashKit
 #endif
+
 struct UserView: View {
   @EnvironmentObject private var environments: ExtensionEnvironments
   @EnvironmentObject var userManager: UserManager
